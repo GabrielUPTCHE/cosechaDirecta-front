9295a04bddd053b66b1f467b4986adaaa47ac305
soporteCamilo7 <soportemeganet7@gmail.com>
2024-04-26 18:25:05 -0500

    Ceacion modulo "detalle producto"

--- cosecha-directa-front/src/app/app-routing.module.ts
+++ cosecha-directa-front/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginViewComponent } from './views/login-view/login-view/login-view.component';
 import { MainPageComponent } from './views/main-page/main-page/main-page.component';
 import { RegisterUserComponent } from './views/register-user/register-user.component';
+import { ProductsViewComponent } from './views/product-details/products-details.component';
 
 const routes: Routes = [
   {path:'', component:LoginViewComponent},
   {path:'pagina-principal', component:MainPageComponent},
-  {path:'registrar-usuario', component:RegisterUserComponent}
+  {path:'registrar-usuario', component:RegisterUserComponent},
+  {path:'products', component:ProductsViewComponent}
 ];
 
 @NgModule({
